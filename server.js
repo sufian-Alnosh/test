b90544299acb565a30ddf30a35cc4db67371111c
sufian-Alnosh <92328207+sufian-Alnosh@users.noreply.github.com>
2024-08-28 19:38:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// تفعيل CORS لجميع المسارات
+app.use(cors());
+
 // خدمة الملفات الثابتة من مجلد 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -24,7 +27,6 @@ app.get('/visitor-info', async (req, res) => {
     try {
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         const response = await axios.get(`https://ipinfo.io/${ip}?token=597d93e989f6ec`);
-        // لقد قمنا بحذف السطر الذي يحتوي على fs.writeFileSync
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching visitor info' });
@@ -35,9 +37,3 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
-
-
-app.use(cors()); // هذا يسمح بطلبات CORS من أي نطاق
-
-// ما تبقى من تعريفات الراوتر والسيرفر
